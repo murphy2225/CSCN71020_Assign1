657b978042b15baf12c4847ba965bfbba7cc117f
murphy2225 <122998544+murphy2225@users.noreply.github.com>
2023-02-08 13:51:41 -0500

    add bug is now fixed.

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -49,7 +49,7 @@ void add() {
 	printf("Enter the third value:");
 	scanf_s("%lf", &num3);
 	result = num1 + num2+ num3;
-	printf("%lf + %lf = %lf\n", num1, num2, result);
+	printf("%lf + %lf + %lf = %lf\n", num1, num2,num3,  result);
 }
 void sub() {
 	double num1, num2, num3, result;
